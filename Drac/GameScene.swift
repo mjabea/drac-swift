7a4d71f9a1fb2c6ce4e8b1366bb7bd6e07ea1b71
Rico Zuniga <hello@ricozuniga.com>
2015-01-21 09:54:36 +0800

    Added rank bitmaps for all resolutions. Some test code to display a card.

--- Drac/GameScene.swift
+++ Drac/GameScene.swift
@@ -11,12 +11,6 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -25,17 +19,22 @@ class GameScene: SKScene {
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let card = SKSpriteNode(color: SKColor.whiteColor(), size: CGSize(width: 49, height: 70))
+            let suit = SKSpriteNode(imageNamed:"heart")
+            let rank = SKSpriteNode(imageNamed:"king-red")
+
+            card.position = location
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            let qWidth = card.frame.width / 4
+            let qHeight = card.frame.height / 4
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            suit.position = CGPoint(x:qWidth, y:qHeight);
+            rank.position = CGPoint(x:-qWidth, y:qHeight);
             
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            self.addChild(card)
+            card.addChild(suit)
+            card.addChild(rank)
+
         }
     }
    
