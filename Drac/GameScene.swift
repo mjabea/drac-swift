5582cf82a7fc29f639c31e0ed05dc02ae2c08d87
Rico Zuniga <hello@ricozuniga.com>
2015-01-23 22:50:15 +0800

    Added basic drag & drop.

--- Drac/GameScene.swift
+++ Drac/GameScene.swift
@@ -9,8 +9,18 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var movingCard:Card? = nil
+    var origPosition = CGPoint()
+    var offset = CGPoint()
+    var topMost = CGFloat(1.0)
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        for i in 1...20 {
+            let card = Card()
+            card.position = CGPoint(x:Int(arc4random_uniform(UInt32(self.frame.width))), y:Int(arc4random_uniform(UInt32(self.frame.height))));
+            self.addChild(card)
+        }
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -18,13 +28,48 @@ class GameScene: SKScene {
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
             
-            let card = Card()
-            card.position = location
-            self.addChild(card)
+            func startMove() {
+                origPosition = (movingCard?.position)!
+                
+                offset = CGPoint(x:origPosition.x - location.x, y:origPosition.y - location.y)
+                movingCard?.zPosition = topMost
+                topMost += 1.0
+            }
+            
+            if self.nodeAtPoint(location) is Card {
+                movingCard = self.nodeAtPoint(location) as? Card
+                startMove()
+            } else if let p = self.nodeAtPoint(location).parent {
+                if p is Card {
+                    movingCard = p as? Card
+                    startMove()
+                }
+            }
         }
     }
     
     override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            movingCard?.position = CGPoint(x:location.x + offset.x, y:location.y + offset.y)
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        if let mc = movingCard {
+            
+            let move = SKAction.moveTo(origPosition, duration: 0.15)
+            mc.runAction(move)
+            movingCard = nil
+        }
+    }
+    
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        if let mc = movingCard {
+            let move = SKAction.moveTo(origPosition, duration: 0.5)
+            mc.runAction(move)
+            movingCard = nil
+        }
     }
    
     override func update(currentTime: CFTimeInterval) {
